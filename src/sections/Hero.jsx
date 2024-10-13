ab355d81dd3135b8fd2fc9703441d9bac61ab2f2
Igwe Clinton <igweclintonmatro@gmail.com>
2024-10-13 12:08:35 +0100

    uodate code and image

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import Button from '../components/Button'
+import { useState } from 'react'
 import { arrowRight } from '../assets/icons'
-import { statistics } from '../constants'
+import { shoes, statistics } from '../constants'
 import { bigShoe1 } from '../assets/images'
+import ShoeCard from '../components/ShoeCard'
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
+
   return (
     <section
     id='home'
@@ -12,8 +15,8 @@ const Hero = () => {
     >
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-green-600'>Our Summer Collection</p>
-        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
-          <span className='xl:bg-white xl:whitespace-nowrap relativez-10 pr-10'>The New Arrival</span>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold relative z-10'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
           <br />
           <span className='text-green-600 inline-block mt-3'>Puma</span> Shoes
         </h1>
@@ -30,12 +33,32 @@ const Hero = () => {
         </div>
       </div>
 
-      <div>
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img 
-        src={bigShoe1}
-        alt="" 
+        src={bigShoeImg}
+        alt="shoes collection"
+        width={610} 
+        height={500}
+        className='object-contain relative z-10'
         />
+
+<div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6' >
+        {shoes.map((shoe) => (
+          <div key={shoe}>
+           <ShoeCard
+           imgURL={shoe}
+           changeBigShoeImage=
+           {(shoe) => setBigShoeImg(shoe)}
+           bigShoeImg={bigShoeImg}
+           />
+           
+          </div>
+        ))}
       </div>
+
+      </div>
+
+    
 
     </section>
   )
